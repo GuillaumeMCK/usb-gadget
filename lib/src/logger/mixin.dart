d21f918603ac80ca0fc9cdd5983c405a11eae4fc
GuillaumeMCK <guillaume.mucwicki@gmail.com>
2025-12-16 17:58:12 +0100

    refactor(logger): add PlatformLogger mixin

--- lib/src/logger/mixin.dart
+++ lib/src/logger/mixin.dart
@@ -6,3 +6,8 @@ mixin USBGadgetLogger implements ILogger {
   @override
   late final Logger? log = _isDebugMode ? .new('$runtimeType') : null;
 }
+
+mixin PlatformLogger implements ILogger {
+  @override
+  late final Logger? log = Logger('$runtimeType');
+}
