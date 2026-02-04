3488a9c091ecda1f9642282a0606c41d613d5888
GuillaumeMCK <guillaume.mucwicki@gmail.com>
2026-02-04 15:48:33 +0100

    refactor(format): Rename xxd.dart to format.dart and add HexString extension

--- lib/src/utils/format.dart
+++ lib/src/utils/format.dart
@@ -1,5 +1,12 @@
 import 'dart:typed_data';
-import 'hex.dart';
+
+extension HexString on int {
+  /// Convert an integer to a hexadecimal string with optional padding.
+  String toHex({int padding = 0, bool prefix = true}) {
+    final hex = toRadixString(16).toUpperCase().padLeft(padding, '0');
+    return prefix ? '0x$hex' : hex;
+  }
+}
 
 extension XXDExt on Uint8List {
   String xxd() {
