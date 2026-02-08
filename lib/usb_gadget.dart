c5c83c417cace100e55be88f30a17ede6d2c7b8b
GuillaumeMCK <guillaume.mucwicki@gmail.com>
2026-02-08 18:28:53 +0100

    chore: Export Errno from platform module

--- lib/usb_gadget.dart
+++ lib/usb_gadget.dart
@@ -1,5 +1,6 @@
 export 'src/functionfs/functionfs.dart';
 export 'src/functions/functions.dart';
 export 'src/gadget/gadget.dart';
+export 'src/platform/platform.dart' show Errno;
 export 'src/usb/usb.dart';
 export 'src/utils/utils.dart';
