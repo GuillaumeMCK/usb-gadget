c304e8c929d3d1a6cbbef5f2c3d307f2b15264fa
GuillaumeMCK <guillaume.mucwicki@gmail.com>
2026-02-22 09:38:57 +0100

    refactor(aio): Remove unused padding fields from io_iocb structs
    Eliminate __pad1 and __pad3 fields from io_iocb_poll and io_iocb_sockaddr
    to clean up the structure definitions.

--- lib/src/platform/aio/aio.ffi.dart
+++ lib/src/platform/aio/aio.ffi.dart
@@ -148,9 +148,6 @@ final class io_context extends ffi.Opaque {}
 final class io_iocb_poll extends ffi.Struct {
   @ffi.Int()
   external int events;
-
-  @ffi.Int()
-  external int __pad1;
 }
 
 final class io_iocb_sockaddr extends ffi.Struct {
@@ -168,9 +165,6 @@ final class io_iocb_common extends ffi.Struct {
 
   @ffi.LongLong()
   external int offset;
-
-  @ffi.LongLong()
-  external int __pad3;
 
   @ffi.UnsignedInt()
   external int flags;
