d1bdbb160636952c0e525713eae028d31688cb76
GuillaumeMCK <guillaume.mucwicki@gmail.com>
2026-03-02 12:12:30 +0100

    feat(logger): Add compile time environment-based logging configuration

--- lib/src/logger/logger.dart
+++ lib/src/logger/logger.dart
@@ -1,3 +1,7 @@
 export 'core.dart';
 export 'mixin.dart';
 export 'printer.dart';
+
+const bool kLogEnabled = .fromEnvironment('USB_GADGET_LOG');
+const bool kLogColors = .fromEnvironment('USB_GADGET_LOG_COLORS');
+const int  kLogLevel = .fromEnvironment('USB_GADGET_LOG_LEVEL', defaultValue: 3);
--- lib/src/logger/mixin.dart
+++ lib/src/logger/mixin.dart
@@ -1,13 +1,11 @@
 import 'logger.dart';
 
-const bool _isDebugMode = .fromEnvironment('USB_GADGET_DEBUG');
-
 mixin USBGadgetLogger implements ILogger {
   @override
-  late final Logger? log = _isDebugMode ? .new('$runtimeType') : null;
+  late final Logger? log = kLogEnabled ? .new('$runtimeType') : null;
 }
 
 mixin PlatformLogger implements ILogger {
   @override
-  late final Logger? log = _isDebugMode ? .new('$runtimeType') : null;
+  late final Logger? log = kLogEnabled ? .new('$runtimeType') : null;
 }
