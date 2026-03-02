22ec4c85608203fdb1cc6c1e61ebfd1719e0f37b
GuillaumeMCK <guillaume.mucwicki@gmail.com>
2026-03-02 15:08:59 +0100

    refactor(logger): Export Logger and remove PlatformLogger

--- lib/usb_gadget.dart
+++ lib/usb_gadget.dart
@@ -1,7 +1,7 @@
 export 'src/functionfs/functionfs.dart';
 export 'src/functions/functions.dart';
 export 'src/gadget/gadget.dart';
-export 'src/logger/logger.dart' show PlatformLogger, USBGadgetLogger, ILogger;
+export 'src/logger/logger.dart' show USBGadgetLogger, ILogger, Logger;
 export 'src/platform/platform.dart' show Errno;
 export 'src/usb/usb.dart';
 export 'src/utils/utils.dart';
