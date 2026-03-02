a030a90f38fa5db03a3b65b8c144896e8ea675e3
GuillaumeMCK <guillaume.mucwicki@gmail.com>
2026-03-02 18:22:11 +0100

    chore: format code

--- lib/src/logger/logger.dart
+++ lib/src/logger/logger.dart
@@ -4,4 +4,4 @@ export 'printer.dart';
 
 const bool kLogEnabled = .fromEnvironment('USB_GADGET_LOG');
 const bool kLogColors = .fromEnvironment('USB_GADGET_LOG_COLORS');
-const int  kLogLevel = .fromEnvironment('USB_GADGET_LOG_LEVEL', defaultValue: 3);
+const int kLogLevel = .fromEnvironment('USB_GADGET_LOG_LEVEL', defaultValue: 3);
