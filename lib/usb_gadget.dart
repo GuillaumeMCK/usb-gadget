178c1ab83ea918eb6e56da258474d084c6b2de48
GuillaumeMCK <guillaume.mucwicki@gmail.com>
2026-03-16 20:12:26 +0100

    refactor(lib): update errno export path
    Update the export path for `Errno` to reflect its new location in `src/platform/errno/errno.dart` and export the entire library instead of just the single class.

--- lib/usb_gadget.dart
+++ lib/usb_gadget.dart
@@ -2,6 +2,6 @@ export 'src/endpoint/endpoint.dart';
 export 'src/function/function.dart';
 export 'src/gadget/gadget.dart';
 export 'src/logger/logger.dart' show USBGadgetLogger, ILogger, Logger;
-export 'src/platform/platform.dart' show Errno;
+export 'src/platform/errno/errno.dart';
 export 'src/usb/usb.dart';
 export 'src/utils/utils.dart';
